9c713cef241b8154ca6781fd3fdb8dd8c4f64a67
Th1nke <th1nkerldm@gmail.com>
2024-10-04 23:14:04 +0500

    latestttt

--- main.py
+++ main.py
@@ -4,24 +4,32 @@ import os
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from app.handlers import router
-# from config.config import TOKEN
 from middlewares.middlewares import AudioFileMiddleware
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
 
+# Get the token
+TOKEN = os.getenv('TOKEN')
+
+# Check if the token is loaded
+if TOKEN is None:
+    raise ValueError("No token found. Please set the TOKEN environment variable.")
+
 # Initialize Bot and Dispatcher
-bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher()
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)  # Pass the bot instance to the Dispatcher
 
 async def main():
     # Include router with your handlers
-    load_dotenv()
     dp.include_router(router)
     dp.update.middleware(AudioFileMiddleware())  # update
     try:
         # Start polling
-        await dp.start_polling(bot)
+        await dp.start_polling()
     finally:
         # Ensure the bot's session is closed on shutdown
         await bot.session.close()
