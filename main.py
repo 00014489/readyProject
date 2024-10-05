6e0c3a9014146bc4a739690a8644ace1ee02bea3
Th1nke <th1nkerldm@gmail.com>
2024-10-05 14:13:32 +0500

    ohirgisi2

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ bot = Bot(token = TOKEN)
 dp = Dispatcher()
 
 async def main():
+    delete_input_songs_folders()
     # Include router with your handlers
     dp.include_router(router)
     dp.update.middleware(AudioFileMiddleware())  # update
@@ -26,6 +27,21 @@ async def main():
         # Ensure the bot's session is closed on shutdown
         await bot.session.close()
         logging.info("Bot session closed.")
+
+def delete_input_songs_folders():
+    # Define the path where the folders are located
+    base_path = '.'  # Change this to the desired base path if needed
+
+    # Iterate over items in the base path
+    for item in os.listdir(base_path):
+        # Check if the item is a directory and starts with 'inputSongs'
+        if os.path.isdir(item) and item.startswith('inputSongs'):
+            try:
+                # Remove the directory and all its contents
+                os.rmdir(item)  # Use shutil.rmtree(item) if you want to delete non-empty directories
+                logging.info(f"Deleted folder: {item}")
+            except Exception as e:
+                logging.error(f"Failed to delete folder {item}: {e}")
 
 if __name__ == '__main__':
     try:
